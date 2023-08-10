a8b346e3935524d077dcbc8f6a01b2af269db00f
NicoLema02 <nicolaslem2@gmail.com>
2023-08-10 09:32:12 -0300

    Trabajo grupal

---
+++ js/validacion.js
@@ -0,0 +1,32 @@
+function showAlertSuccess() {
+    document.getElementById("alert-success").classList.add("show");
+}
+
+function showAlertError() {
+    document.getElementById("alert-danger").classList.add("show");
+}
+
+
+
+
+
+
+regBtn.addEventListener('click', function () {
+    var nombre = document.getElementById('nombre').value;
+    var apellido = document.getElementById('apellido').value;
+    var contrasena = document.getElementById('password1').value;
+    var email = document.getElementById('email').value;
+    var repetirContra = document.getElementById('password2').value;
+    console.log(contrasena);
+      
+if (nombre.length  > 0 && apellido.length  > 0 && contrasena.length   > 0 && email.length   > 0 && repetirContra.length   > 0 && contrasena.length > 5 && contrasena === repetirContra && document.getElementById('terminos').checked)  {
+    
+        showAlertSuccess();
+    }
+    else { 
+        
+        showAlertError();
+    }
+});
+
+
